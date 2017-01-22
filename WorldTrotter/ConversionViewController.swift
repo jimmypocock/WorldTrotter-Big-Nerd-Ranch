d1449529c1554fdf9785e62e2fbb6561c9c4496a
Jimmy Pocock <jimmypocock@yahoo.com>
2017-01-22 16:15:34 -0600

    localized some things for spanish

--- WorldTrotter/ConversionViewController.swift
+++ WorldTrotter/ConversionViewController.swift
@@ -35,8 +35,8 @@ class ConversionViewController: UIViewController, UITextFieldDelegate {
     }
 
     @IBAction func fahrenheitFieldEditingChanged(_ textField: UITextField) {
-        if let text = textField.text, let value = Double(text) {
-            fahrenheitValue = Measurement(value: value, unit: .fahrenheit)
+        if let text = textField.text, let number = numberFormatter.number(from: text) {
+            fahrenheitValue = Measurement(value: number.doubleValue, unit: .fahrenheit)
         } else {
             fahrenheitValue = nil
         }
@@ -61,13 +61,16 @@ class ConversionViewController: UIViewController, UITextFieldDelegate {
 
     func textField(_ textField: UITextField,shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
 
-        let existingTextHasDecimalSeparator = textField.text?.range(of: ".")
-        let replacementTextHasDecimalSeparator = string.range(of: ".")
+        let currentLocale = Locale.current
+        let decimalSeparator = currentLocale.decimalSeparator ?? "."
+
+        let existingTextHasDecimalSeparator = textField.text?.range(of: decimalSeparator)
+        let replacementTextHasDecimalSeparator = string.range(of: decimalSeparator)
 
         if existingTextHasDecimalSeparator != nil && replacementTextHasDecimalSeparator != nil {
             return false
         } else {
-            if !isDecimalDigit(string: string) && string != "." {
+            if !isDecimalDigit(string: string) && replacementTextHasDecimalSeparator =gs= nil {
                 return false
             }
             return true
